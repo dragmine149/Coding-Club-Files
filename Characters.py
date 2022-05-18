47e5743bb5c5a11ac636715a9326724f458bad78
dragmine149-Termux <dan@thebanners.uk>
2022-05-18 14:02:58 +0100

    Updated: 18/05/22

--- Characters.py
+++ Characters.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 # Make a new class for the character
 class character:
@@ -8,14 +9,15 @@ class character:
         self.name = name
         self.strength = strength
         self.health = health
+        self.alive = True
 
     # Returns the character name
     def getName(self):
         return self.name
 
     # Retruns the character strength
-    def getStrenght(self):
-        return self.strenght
+    def getStrength(self):
+        return self.strength
     
     # Returns the character health
     def getHealth(self):
@@ -24,6 +26,8 @@ class character:
     # Changes the character health by X
     def changeHealth(self, ammount):
         self.health += ammount
+        if self.health <= 0:
+            self.alive = False
 
 
 sayu = character('sayu', 10, 100)
@@ -43,3 +47,30 @@ print(f'{sayu.getName()}! You got hit!')
 health = random.randint(1,6)
 sayu.changeHealth(-health)
 print(f'{sayu.getName()} Your health is now {sayu.getHealth()}')
+
+def tree():
+    answer = None
+    while answer is None:
+        answer = input("You see a tree! Do you attack or hug?: ")
+        if answer.lower() == "hug":
+            print("The tree healed you!")
+            sayu.Health = 100
+        elif answer.lower() == "attack":
+            print("The tree attacked back!")
+            sayu.changeHealth(-1)
+        else:
+            print("Please enter a valid input")
+
+def qiqiE():
+    while qiqi.alive:
+        print("You spot a wild qiqi")
+        print("You automaticall attack qiqi")
+        sayu.changeHealth(-qiqi.getStrength())
+        qiqi.changeHealth(-sayu.getStrength())
+
+encourtments = [tree, qiqiE]
+
+os.system('clear')  # get rid of old stuff, not needed.
+while sayu.alive:
+    item = random.randint(0, len(encourtments))
+    encourtments[item -1]()
